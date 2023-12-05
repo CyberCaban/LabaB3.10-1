b9f17728b8423bc60fb9c8d086e0a2c8abee5acd
Dmitry <andrewgamer63@gmail.com>
2023-12-05 22:00:00 +0300

    fineal

--- LabaB3.10-1/Source.c
+++ LabaB3.10-1/Source.c
@@ -1,16 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#define WORD_LEN 20
-
-char SentEnd[3] = { '.', '!', '?' };
 
 typedef struct Node {
-	int val;
+	char* word;
 	struct Node* next;
 } TNode;
 
-void pushEnd(TNode** head, int data) {
+void pushEnd(TNode** head, char* word) {
 	TNode* tmp = (TNode*)malloc(sizeof(TNode));
 
 	if (tmp == NULL) {
@@ -20,13 +17,13 @@ void pushEnd(TNode** head, int data) {
 
 	TNode* curr = (*head);
 	if (curr == NULL) {
-		tmp->val = data;
+		tmp->word = word;
 		(*head) = tmp;
 	}
 	else {
 		for (; curr; curr = curr->next) {
 			if (curr->next == NULL) {
-				tmp->val = data;
+				tmp->word = word;
 				curr->next = tmp;
 				return;
 			}
@@ -34,21 +31,102 @@ void pushEnd(TNode** head, int data) {
 	}
 }
 
-int sentenceInput(char* sentence) {
-	char c;
+int sentenceInput(TNode** sentence) {
+	char* word = NULL;
+	char ch, t;
 	int i = 0;
-	while ((c = getchar()) != '.' && c != '!' && c != '?') {
-		sentence[i] = c;
+
+	for (; (ch = getchar()) != '.' && ch != '!' && ch != '?';) {
+		if (!((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || ch == ' ')) {
+			printf("Non valid symbol");
+			return 0;
+		}
+		if (ch == ' ') {
+			if (word) {
+				word[i] = '\0';
+				pushEnd(sentence, word);
+				word = NULL;
+				i = 0;
+			}
+			continue;
+		}
+
+		word = (char*)realloc(word, i + 1);
+		word[i] = ch;
 		i++;
+
+		if (i >= 20) {
+			printf("You cannot input words more than 20 symbols in it");
+			return 0;
+		}
+	}
+
+	//if (!i) { // if entered only '.'
+	//	printf("Empty sentence");
+	//	return 0;
+	//}
+	if (word) {
+		word[i] = '\0';
+		pushEnd(sentence, word);
+		scanf("%c", &t);
+	}
+
+	return 1;
+}
+
+int strLength(char* str) {
+	int i = 0;
+	for (; str[i]; i++) {
+	}
+	return i;
+}
+
+void sentenceOutput(TNode** sentence, int k) {
+	for (TNode* curr = (*sentence); curr; curr = curr->next) {
+		int strBegin = k - strLength(curr->word);
+		for (int i = 0; i < k; i++) {
+			if (i < strBegin) {
+				printf(" ");
+			}
+			else {
+				printf("%s", curr->word);
+				break;
+			}
+		}
+		printf("\n");
+	}
+}
+
+void listFree(TNode** head) {
+	if ((*head) == NULL) {
+		return;
+	}
+
+	TNode* next;
+	for (; (*head);) {
+		next = (*head)->next;
+		free((*head));
+		(*head) = next;
 	}
 }
 
 int main() {
 	TNode* sentence = NULL;
-	char word[WORD_LEN];
+	int k;
 
-	sentenceInput(word);
-	printf("%s", word);
-	/*scanf("%s", word);
-	printf("%s", word);*/
+	printf("Enter sentence:\n");
+	if (!sentenceInput(&sentence)) {
+		return 1;
+	}
+
+	printf("Enter right pos:\n");
+	scanf("%d", &k);
+	if (k < 20 || k > 80) {
+		printf("Enter valid right pos. It must be integer more than 19 and less "
+			"than 81");
+		return 1;
+	}
+
+	sentenceOutput(&sentence, k);
+	listFree(&sentence);
 }
